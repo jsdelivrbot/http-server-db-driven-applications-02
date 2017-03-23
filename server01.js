da81588a2b478a6ec90ca3d73f30f65fd7bbd60e
Wisbell <westleyisbell@gmail.com>
2017-03-23 09:56:07 -0500

    Finished writing server to read a readStream and respond with an html file

--- server01.js
+++ server01.js
@@ -1,13 +1,24 @@
+// require functions
 const { createServer } = require('http');
 const { createReadStream } = require('fs');
 
+// google brought me here
+// https://docs.nodejitsu.com/articles/advanced/streams/how-to-use-fs-create-read-stream/
+
+// set up server
 const server = createServer();
 
 server.on('request', (req, res) => {
   console.log('Request received for: ', req.url)
-  res.end('OK')
 
+  // read stream needs to be in the server request
+  const readStream = createReadStream('index.html')
 
+  // on request read the file stream and pipe it to
+  // the response object
+  readStream.on('open',(buff) => {
+    readStream.pipe(res)
+  })
 })
 
 server.listen(8080)
